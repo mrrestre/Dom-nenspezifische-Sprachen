41c571f760f4cebd3e4038292c2cce73525e8fb8
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2024-10-24 13:22:39 +0200

    better implementation of interpreter :)

--- interpreter.py
+++ interpreter.py
@@ -3,64 +3,82 @@ import json
 class Interpreter:
     def __init__(self, path):
         self.path = path
-        self.statements = []
-        self.load_compiled()
+        self.variables = {}
+        self.ast = {}
+        self.load_ast()
 
-        self.functions = {
-            'WRITE': self.do_write,
-        }
-        self.expressions = {
-            'STRTOKEN': self.strtoken,
-            'NUMTOKEN': self.numtoken,
-            'PLUS': self.plus,
+        self.node_type_handlers = {
+            "STATEMENTBLOCK": self.handle_statement_block,
+
+            "ASSIGN": self.handle_assign,
+            "IDENTIFIER": self.handle_identifier,
+            "WRITE": self.handle_write,
+            
+            "PLUS": self.handle_plus,
+            "MINUS": self.handle_minus,
+
+            "STRTOKEN": self.handle_str_token,
+            "NUMTOKEN": self.handle_num_token,
         }
 
-    def load_compiled(self):
+    def load_ast(self):
         with open(self.path, 'r') as file:
-            compiled = json.load(file)
-            if compiled['type'] == 'STATEMENTBLOCK':
-                self.statements = compiled['statements']
-            else:
-                raise ValueError("Invalid code structure")
+            self.ast = json.load(file)
 
     def interpret(self):
-        for statement in self.statements:
-            self.process_statement(statement)
+        ast = self.parse_node(self.ast)
+        self.visit(ast)
 
-    def process_statement(self, statement):
-        statement_type = statement['type']
-        # Search for the function in the library
-        function = self.functions.get(statement_type)
-        if function:
-            # Execute the function with args
-            function(statement['arg'])
-        else:
-            print(f"Unknown statement type: {statement_type}")
+    def parse_node(self, node_data):
+        node_type = node_data["type"]
+        if node_type not in self.node_type_handlers:
+            raise ValueError(f"Unknown node type: {node_type}")
+        return node_data
 
-    def do_write(self, arg):
-        arg_type = arg['type']
-        expression = self.expressions.get(arg_type)
-        if expression:
-            if arg_type == 'PLUS':
-                result = expression(arg['arg'])
-                print(result)
-            else:
-                print(expression(arg))
-        else:
-            print(f"Unknown argument type: {arg_type}")
+    def visit(self, node):
+        node_type = node["type"]
+        handler = self.node_type_handlers.get(node_type)
+        if not handler:
+            raise ValueError(f"Handler not found for node type: {node_type}")
+        return handler(node)
 
-    def strtoken(self, arg):
-        return arg['value']
+    def handle_statement_block(self, node):
+        for statement in node["statements"]:
+            self.visit(statement)
 
-    def numtoken(self, arg):
-        return arg['value']
+    def handle_assign(self, node):
+        result = self.visit(node["arg"])
+        self.variables.update({node['varname']: result})
+    
+    def handle_identifier(self, node):
+        variable_name = self.variables.get(node['name'])
+        if variable_name:
+            return variable_name
+        else: 
+            return 'Variable ' + node['name'] + ' in line ' + node['line'] + ' not defined in scope'
 
-    def plus(self, args):
-        total = 0
-        for arg in args:
-            arg_type = arg['type']
-            if arg_type == 'NUMTOKEN':
-                total += float(arg['value'])
-            else:
-                print(f"Unknown argument type in PLUS: {arg_type}")
-        return total
+    def handle_write(self, node):
+        result = self.visit(node["arg"])
+        print(result)
+
+
+    def handle_plus(self, node):
+        args = node["arg"]
+        return self.visit(args[0]) + self.visit(args[1])
+    
+    def handle_minus(self, node):
+        args = node["arg"]
+        return self.visit(args[0]) - self.visit(args[1])
+    
+
+    def handle_str_token(self, node):
+        return node["value"]
+
+    def handle_num_token(self, node):
+        return float(node["value"])
+
+
+
+
+
+
